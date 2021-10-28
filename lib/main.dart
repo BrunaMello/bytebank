577120a7638641972bc99535d4a9499be22a531b
Bruna Mello <brunacrespomello@gmail.com>
2021-10-28 17:46:14 +0100

    02 - Implementando layout com Widgets
    Nesta aula, aprendemos:
    
    A implementar um layout com os componentes do Material Design;
    Atalhos e dicas para auxiliar a implementação do layout;
    Refatorar os próprios Widgets;
    O que é StatelessWidget e StatefulWidget;
    Organizar e reutilizar código no Flutter.

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,52 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-    Column(
-      children: <Widget>[
-        Text('Deliver features faster', textDirection: TextDirection.ltr,),
-        Text('Craft beautiful UIs', textDirection: TextDirection.ltr,),
-        Text('Craft beautiful UIs', textDirection: TextDirection.ltr,),
-        Column(
-          children: <Widget>[
-            Text('Deliver features faster', textDirection: TextDirection.ltr,),
-          ],
-        )
-      ],
+void main() => runApp(MaterialApp(
+      home: Scaffold(
+        body: TransfersLists(),
+        appBar: AppBar(
+          title: Text('Transfers'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.add),
+        ),
+      ),
+    ));
 
-    ),
-);
+class TransfersLists extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        TransferItem(Transfer(100.0, 123)),
+        TransferItem(Transfer(400.0, 321)),
+        TransferItem(Transfer(500.0, 666)),
+      ],
+    );
+  }
+}
+
+class TransferItem extends StatelessWidget {
+
+  final Transfer _transfer;
+
+  TransferItem(this._transfer);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(_transfer.valor.toString()),
+        subtitle: Text(_transfer.accountnumber.toString()),
+      ),
+    );
+  }
+}
+
+class Transfer {
+  final double valor;
+  final int accountnumber;
+
+  Transfer(this.valor, this.accountnumber);
+}
