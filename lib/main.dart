8ae6ac8463984375f2753fdf53ddb3dcd0ec8be5
Bruna Mello <brunacrespomello@gmail.com>
2021-10-28 02:24:05 +0100

    01 - Primeiros Passos com o Flutter Nesta aula, aprendemos: O que é o Flutter; Como instalar e preparar o ambiente para o Flutter; Criar e executar o primeiro projeto; Abrir o projeto no IntelliJ, editar o código e executá-lo; O que são Widgets e como funciona a estrutura para criar layouts no Flutter.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(Text('Bem vindo ao Bytebank', textDirection: TextDirection.ltr,));
+void main() => runApp(
+    Column(
+      children: <Widget>[
+        Text('Deliver features faster', textDirection: TextDirection.ltr,),
+        Text('Craft beautiful UIs', textDirection: TextDirection.ltr,),
+        Text('Craft beautiful UIs', textDirection: TextDirection.ltr,),
+        Column(
+          children: <Widget>[
+            Text('Deliver features faster', textDirection: TextDirection.ltr,),
+          ],
+        )
+      ],
 
-
-
-
+    ),
+);
