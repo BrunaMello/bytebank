070c44e70eb81712b5436afaf1e6ee7c73befabe
Bruna Mello <brunacrespomello@gmail.com>
2021-10-29 22:37:13 +0100

    03 - Criando formulario e refatorando o codigo
    Nesta aula, aprendemos:
    Maneiras de evitar Widgets inesperados para outras telas;
    Como habilitar e utilizar o Hot Reload;
    Criar um layout para formulário;
    Aplicar margem em Widgets;
    Como pegar o valor e estilizar TextField.

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,94 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-      home: Scaffold(
-        body: TransfersLists(),
-        appBar: AppBar(
-          title: Text('Transfers'),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: Icon(Icons.add),
-        ),
-      ),
-    ));
+void main() => runApp(BytebankApp());
 
-class TransfersLists extends StatelessWidget {
+class BytebankApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        TransferItem(Transfer(100.0, 123)),
-        TransferItem(Transfer(400.0, 321)),
-        TransferItem(Transfer(500.0, 666)),
-      ],
+    return MaterialApp(
+      home: Scaffold(
+        body: TransferForm(),
+      ),
+    );
+  }
+}
+
+
+class TransferForm extends StatelessWidget{
+
+  final TextEditingController _controllerAccountField = TextEditingController();
+  final TextEditingController _controllerValueField = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Create Transfer'),),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              controller: _controllerAccountField,
+              style: TextStyle(
+                fontSize: 24.0,
+              ),
+              decoration: InputDecoration(
+                labelText: 'Account Number',
+                hintText: '0000'
+              ),
+              keyboardType: TextInputType.number,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              controller: _controllerValueField,
+              style: TextStyle(
+                fontSize: 24.0,
+              ),
+              decoration: InputDecoration(
+                icon: Icon(Icons.monetization_on),
+                  labelText: 'Value',
+                  hintText: '0.00'
+              ),
+              keyboardType: TextInputType.number,
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              final int accountNumber = int.parse(_controllerAccountField.text);
+              final double value = double.parse(_controllerValueField.text);
+              if(accountNumber != null && value != null){
+                final createdTransfer = Transfer(value, accountNumber);
+                debugPrint('$createdTransfer');
+              }
+              } ,
+            child: Text('Confirm'),
+          )
+        ],
+      ));
+}
+}
+
+class TransfersLists extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+        title: Text('Transfers'),
+        ),
+      body: Column(
+        children: <Widget>[
+          TransferItem(Transfer(100.0, 123)),
+          TransferItem(Transfer(400.0, 321)),
+          TransferItem(Transfer(500.0, 666)),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
@@ -49,4 +116,9 @@ class Transfer {
   final int accountnumber;
 
   Transfer(this.valor, this.accountnumber);
+
+  @override
+  String toString() {
+    return 'Transfer{valor: $valor, accountnumber: $accountnumber}';
+  }
 }
